2f7957c265d0130abdcead4a9ee117ddb310e2f0
jh20s <jh20s@naver.com>
2021-04-04 17:04:06 +0900

    feat(edit): edit player can't go map outside

--- ShootingGame/Assets/Scripts/PlayerMove.cs
+++ ShootingGame/Assets/Scripts/PlayerMove.cs
@@ -18,5 +18,16 @@ public class PlayerMove : MonoBehaviour
         
         Vector3 dir = new Vector3(h, v, 0);
         transform.position += dir * speed * Time.deltaTime;
+
+
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        if (pos.x < 0f) pos.x = 0f;
+        if (pos.x > 1f) pos.x = 1f;
+        if (pos.y < 0f) pos.y = 0f;
+        if (pos.y > 1f) pos.y = 1f;
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+
+
+
     }
 }
